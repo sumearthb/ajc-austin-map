d1897b63ae4bce2ff344bcb3c820f36a61aa2ce3
nathanjacob <37455968+nathan-jacob@users.noreply.github.com>
2025-03-22 22:16:17 -0500

    Nathan Part done. API key is public rn prolly shouldn't be like that

--- map/src/App.js
+++ map/src/App.js
@@ -2,53 +2,99 @@ import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
 import Checkbox from '@mui/material/Checkbox';
-import FormGroup from '@mui/material/FormGroup';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-
-
+import Select from '@mui/material/Select';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [type, setType] = useState('');
+  const [places, setPlaces] = useState([]);
 
-  const [type, setType] = React.useState('');
+  useEffect(() => {
+    fetch('places.json') 
+      .then(response => response.json())
+      .then(data => {
+        setPlaces(data.places);
+      })
+      .catch(error => console.error('Error loading places:', error));
+  }, []);
 
   const handleChange = (event) => {
     setType(event.target.value);
   };
 
+  const initMap = () => {
+    const austin = { lat: 30.2672, lng: -97.7431 };
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: austin,
+    });
+
+    places.forEach((place) => {
+      const marker = new window.google.maps.Marker({
+        position: { lat: place.latitude, lng: place.longitude },
+        map: map,
+        title: place.name,
+      });
+
+      const infoWindow = new window.google.maps.InfoWindow({
+        content: `<h3>${place.name}</h3><p>${place.description}</p>`,
+      });
+
+      marker.addListener('click', () => {
+        infoWindow.open(map, marker);
+      });
+    });
+  };
+
+  useEffect(() => {
+    if (window.google && window.google.maps) {
+      initMap();
+    }
+  }, [places]);
+
   return (
     <div className="App">
-      <div class="main_card">
-        <div class="title">Austin Recommendations Map</div>
-        <div class="tool_card">
-          <div class="left-col">
-            <FormControlLabel class="check_container" control={<Checkbox sx={{ p: .5 }}/>} label="Kid-Friendly" />
-            <FormControlLabel class="check_container" control={<Checkbox sx={{ p: .5 }}/>} label="Open Now" />
-            <FormControlLabel class="check_container" control={<Checkbox sx={{ p: .5 }}/>} label="Free" />
+      <div className="main_card">
+        <div className="title">Austin Recommendations Map</div>
+        <div className="tool_card">
+          <div className="left-col">
+            <FormControlLabel
+              className="check_container"
+              control={<Checkbox sx={{ p: 0.5 }} />}
+              label="Kid-Friendly"
+            />
+            <FormControlLabel
+              className="check_container"
+              control={<Checkbox sx={{ p: 0.5 }} />}
+              label="Open Now"
+            />
+            <FormControlLabel
+              className="check_container"
+              control={<Checkbox sx={{ p: 0.5 }} />}
+              label="Free"
+            />
           </div>
-          <div class="right-col">
-          <div class = "right_container">Location Type</div>
-          <FormControl fullWidth>
-           {/* <InputLabel id="type_label">Location Type</InputLabel> */}
-            <Select
-            // labelId="type_label"
-            id="type_select"
-            value = {type}
-            // label="Location Type"
-            onChange={handleChange}
-            >
-            <MenuItem value="Restraunts">Restaraunts</MenuItem>
-            <MenuItem value="Grocery Stores">Grocery Stores</MenuItem>
-            <MenuItem value="Parks">Parks</MenuItem>
-            </Select>
+          <div className="right-col">
+            <div className="right_container">Location Type</div>
+            <FormControl fullWidth>
+              <Select
+                id="type_select"
+                value={type}
+                onChange={handleChange}
+              >
+                <MenuItem value="Restaurants">Restaurants</MenuItem>
+                <MenuItem value="Grocery Stores">Grocery Stores</MenuItem>
+                <MenuItem value="Parks">Parks</MenuItem>
+                <MenuItem value="Cultural">Cultural</MenuItem>
+              </Select>
             </FormControl>
           </div>
         </div>
-        <div class="map_container">
-            <img class="map" src='https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2023/05/google-maps-icon-on-map.jpg' alt='Map'></img>
+        <div className="map_container">
+          <div id="map" style={{ width: '100%', height: '500px' }}></div>
         </div>
       </div>
     </div>
