e10ba3918d32f5c74769d2ad8101b8d18d3d3aed
Sophia Fisher <sophia.anne.fisher@gmail.com>
2025-03-25 18:22:04 -0500

    small changes to tab header

--- map/setup.sh
+++ map/setup.sh
@@ -13,9 +13,12 @@ then
 fi
 
 echo "Installing project dependencies..."
-npm install
 
+npm install
 npm install @react-google-maps/api
+npm install @mui/material
+npm install @emotion/react @emotion/styled
+npm install -g gh-pages
 
 echo "Dependencies installed successfully."
 
